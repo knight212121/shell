5b704d5d6db4abd93b8e1ecfdc6e2d11e5ca74e8
knight21 <knight211221@gmail.com>
2025-12-04 21:56:57 +0530

    Implement type

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,8 +1,10 @@
 #include "executor.h"
 #include "../builtins/builtins.h"
+#include <stdio.h>
 
 void execute_command(CommandArgs* cmd) {
     if (execute_builtin_command(cmd) == 1) {
         return;
     }
+    printf("%s: command not found", cmd -> argv[0]);
 }
