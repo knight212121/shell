5f496904fe0ad70cd3614b9932ce8d11e934d336
knight21 <knight211221@gmail.com>
2025-12-04 11:21:21 +0530

    Implement exit

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+void exit_shell(char* status) {
+    int code = atoi(status);
+    exit(code);
+}
 
 int main(int argc, char *argv[]) {
     while (1) {
@@ -12,6 +18,17 @@ int main(int argc, char *argv[]) {
         fgets(command, sizeof(command), stdin);
 
         command[strcspn(command, "\n")] = '\0';
+
+        char *rest = command, *token;
+        char *executable = strtok_r(rest, " ", &rest), *args;
+
+        while((token = strtok_r(rest, " ", &rest))) {
+            args = token;
+        }
+
+        if (strcmp(executable, "exit") == 0) {
+            exit_shell(args);
+        }
 
         printf("%s: command not found\n", command);
     }
