e28e70c5f905ef6a02a08726b3d58a9ddf19a309
knight212121 <knight211221@gmail.com>
2025-12-22 23:01:24 +0530

    Dual-command pipeline

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -97,10 +97,6 @@ void execute_command(Pipeline *pipes) {
                     file_desc =
                         open(cmd->stdout_file, O_CREAT | O_APPEND | O_WRONLY,
                              S_IRUSR | S_IWUSR);
-                if (file_desc == -1) {
-                    perror(cmd->stdout_file);
-                    _exit(1);
-                }
                 dup2(file_desc, STDOUT_FILENO);
                 close(file_desc);
             }
@@ -114,10 +110,6 @@ void execute_command(Pipeline *pipes) {
                     file_desc =
                         open(cmd->stderr_file, O_CREAT | O_APPEND | O_WRONLY,
                              S_IRUSR | S_IWUSR);
-                if (file_desc == -1) {
-                    perror(cmd->stderr_file);
-                    _exit(1);
-                }
                 dup2(file_desc, STDERR_FILENO);
                 close(file_desc);
             }
