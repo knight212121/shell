0a70b50c8f964eb28158842f6e81069e43343273
knight212121 <knight211221@gmail.com>
2025-12-25 11:18:19 +0530

    Up-arrow navigation

--- src/builtins/history.c
+++ src/builtins/history.c
@@ -1,39 +1,22 @@
 #include "history.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <readline/history.h>
+#include <stdio.h>
 #include <string.h>
 
 History *history;
 
-void initialize_history(int size) {
-    history = malloc(sizeof(History *));
-    history->max_size = size;
-    history->begin = 0;
-    history->lines = malloc(sizeof(char *) * size);
-    history->insertion_count = 0;
-}
-
-void add_to_history(char *command) {
-    if (history->begin >= history->max_size) {
-        history->max_size *= 2;
-        char **temp = realloc(history->lines, sizeof(char *) * history->max_size);
-        if (!temp)
-            return;
-        history->lines = temp;
-    }
-    history->lines[history->begin++] = strdup(command);
-    history->insertion_count += 1;
-}
-
 void print_history(int offset) {
-    int i;
-    if (offset == -1)
-        i = 0;
-    else
-        i = history->insertion_count - offset;
-    while(i < history->insertion_count) {
-        if(history->lines[i] != NULL)
-            printf("    %d  %s\n", i + 1, history->lines[i]);
-        i += 1;
+    if (offset == -1) {
+        HIST_ENTRY **hist = history_list();
+        for (int i = 0; hist[i] != NULL; i += 1) {
+            printf("    %d  %s\n", i + 1, hist[i]->line);
+        }
+    } else {
+        for (int i = 0; i < offset; i += 1) {
+            HIST_ENTRY *entry = history_get(where_history() - offset + 2 + i);
+            if (entry)
+                printf("    %d  %s\n", where_history() - offset + i + 2, entry->line);
+        }
     }
 }
--- src/main.c
+++ src/main.c
@@ -1,33 +1,46 @@
 #include "autocomplete/autocomplete.h"
-#include "builtins/history.h"
 #include "executor/executor.h"
 #include "input/input.h"
 #include "utils/common.h"
 #include <stdio.h>
+#include <readline/history.h>
 #include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+
+static volatile int keep_running = 1;
+
+void int_handler(int dummy) {
+    keep_running = 0;
+}
 
 int main(int argc, char *argv[]) {
     create_autocomplete_cache();
     rl_attempted_completion_function = input_completion;
-    initialize_history(1);
-    while (1) {
+    using_history();
+    read_history("/tmp/history");
+    signal(SIGINT, int_handler);
+    while (keep_running) {
         setbuf(stdout, NULL);
 
         char *input = readline("$ ");
 
+        if (!input)
+            break;
+
+        if (*input)
+            add_history(input);
+
         Pipeline *pipes = tokenize_input(input);
 
         if (pipes != NULL) {
-            add_to_history(input);
-
             execute_command(pipes);
+            free_command_args(pipes);
         }
-        free_command_args(pipes);
-
         free(input);
     }
+    write_history("/tmp/history");
 
     return 0;
 }
